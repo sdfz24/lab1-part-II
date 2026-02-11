1db1523d29854b1ee4820a83b80483b50cf877ec
rodriibalondeoro <gfxrodrii@gmail.com>
2026-02-11 09:52:41 +0100

    Add Provider

--- billing/api/serializers.py
+++ billing/api/serializers.py
@@ -4,9 +4,15 @@ from ..models import Provider, Barrel, Invoice, InvoiceLine
 
 
 class ProviderSerializer(serializers.ModelSerializer):
+    barrel_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        read_only=True,
+        source='barrels'
+    )
+
     class Meta:
         model = Provider
-        fields = ["id", "name", "address", "tax_id"]
+        fields = ["id", "name", "address", "tax_id", "barrel_ids"]
 
 
 class BarrelSerializer(serializers.ModelSerializer):
